39c4427ccf329ad78a0e3754554aa2ef998470e8
VIETTELGROUP\os_spgp_gpdn271 <tranduongkyoto@gmail.com>
2022-04-06 15:20:12 +0700

    add config-server and test

---
+++ greetings/src/main/java/tranduongkyoto/greetings/GreetingProps.java
@@ -0,0 +1,20 @@
+package tranduongkyoto.greetings;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "greeting")
+@Component
+public class GreetingProps {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
---
+++ greetings/src/test/java/tranduongkyoto/greetings/GreetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package tranduongkyoto.greetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
